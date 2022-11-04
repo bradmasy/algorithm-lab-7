5e44122ca402a04574e928b02d770e2ff4529ff8
Brad <bmasciotra@my.bcit.ca>
2022-11-04 09:56:11 -0700

    comments added, working test graphs

--- src/Graph.java
+++ src/Graph.java
@@ -1,4 +1,20 @@
-import java.lang.reflect.Array;
+/**
+ * Lab 7: Data Structures and Graphs.
+ *
+ * @name: Bradley Masciotra.
+ * @student_number: A01247718
+ * @date: November 4th 2022
+ * @version: 1.0
+ *
+ * @description:
+ *
+ * Lab 7 involves creating an adjacency matrix based on a corresponding graph where the numerical value of 1
+ * represents an edge in the graph. The file contains methods for a Depth first search(DFS) and Breadth first search(BFS)
+ * algorithms to find the correct corresponding path. Graphs have the option to be direction or not which will
+ * impact the given functions for inDegree and outDegree which measure the amount of incoming edges and outgoing edges.
+ *
+ */
+
 import java.util.ArrayList;
 
 public class Graph {
@@ -6,10 +22,29 @@ public class Graph {
     int[][] adjacencyMatrix;
     boolean directed;
 
+    /**
+     * Graph constructor.
+     *
+     * Creates an adjacency matrix based on the value v passed.
+     *
+     * @param v an integer representing the dimensions of the adjacency matrix.
+     */
     public Graph(final int v) {
         adjacencyMatrix = new int[v][v];
         directed = false;
+    }
 
+    /**
+     * Alternative graph constructor.
+     *
+     * Creates an adjacency matrix based on the value v passed and whether the graph is directed or not.
+     *
+     * @param v an integer representing the dimensions of the adjacency matrix.
+     * @param directed boolean representing whether the graph is directed or not.
+     */
+    public Graph(final int v, final boolean directed) {
+        adjacencyMatrix = new int[v][v];
+        this.directed = directed;
     }
 
     ;
@@ -27,12 +62,16 @@ public class Graph {
         }
 
         adjacencyMatrix[v][u] = 1;
-
     }
 
+    /**
+     * Gets the degrees to the given vertex provided.
+     *
+     * @param v the vertex we are inquiring about.
+     * @return the number of degrees/edges pertaining to the vertex.
+     */
     public int degree(final int v) {
         int degree = 0;
-
 
         for (int eachEdge : adjacencyMatrix[v]) {
             if (eachEdge == 1) {
@@ -40,19 +79,32 @@ public class Graph {
             }
         }
 
-
         return degree;
     }
 
+    /**
+     * Sets the direction of the graph.
+     *
+     * @param directed a boolean representing whether the graph is directed or not.
+     */
     public void setDirection(boolean directed) {
         this.directed = directed;
     }
 
+    /**
+     * Gets the direction of the graph, whether it is directed or not.
+     *
+     * @return the graph's direction.
+     */
     public boolean getDirection() {
         return this.directed;
     }
 
-
+    /**
+     * Returns the adjacency matrix transposed as a string of the graph.
+     *
+     * @return the adjacency matrix transposed as a string of the graph.
+     */
     @Override
     public String toString() {
         StringBuilder data = new StringBuilder();
@@ -75,8 +127,13 @@ public class Graph {
         return data.toString();
     }
 
+    /**
+     * gets the amount of incoming degrees into the given vertex.
+     *
+     * @param v a given vertex.
+     * @return the amount of incoming degrees on a given vertex.
+     */
     int inDegree(int v) {
-        // anything coming into v
         int degree = 0;
         for (int[] eachRow : adjacencyMatrix) {
             if (eachRow[v] == 1) {
@@ -86,6 +143,12 @@ public class Graph {
         return degree;
     }
 
+    /**
+     * gets the amount of outgoing degrees into the given vertex.
+     *
+     * @param v a given vertex.
+     * @return the amount of outgoing degrees on a given vertex.
+     */
     int outDegree(int v) {
         int outDegree = 0;
 
@@ -97,6 +160,13 @@ public class Graph {
         return outDegree;
     }
 
+    /**
+     * Traverses the vertexes given neighbours and returns the next vertex to advance to.
+     *
+     * @param vertex the vertex we are analyzing.
+     * @param visitedNodes the list of visited nodes.
+     * @return the next vertex to traverse to.
+     */
     public int traverseNodes(int vertex, ArrayList<Integer> visitedNodes) {
         int tempVertex;
 
@@ -138,6 +208,11 @@ public class Graph {
         return traverseNodes(vertex, visitedNodes);
     }
 
+    /**
+     * Traverses the nodes in the DFS algorithm recursively building the given visited list based on visiting
+     * the next available neighbouring vertex with an edge, if none is available the algorithm backtracks to a
+     * neighbour with an available neighbour if none is available the algorithm is complete.
+     */
     public void DFS() {
         int startingVertexIndex = 0;
         int visited = -1;
@@ -152,6 +227,12 @@ public class Graph {
     }
 
 
+    /**
+     * Traverses the nodes based on a BFS algorithm. Starting at a given vertex we traverse the adjacency matrix
+     * based on the vertexes available neighbours. The algorithm travels to a given vertex, records all its neighbours,
+     * and when all the neighbours have been visited moves to the first neighbour from the stack until the traversal
+     * is complete.
+     */
     public void BFS(){
         int startingVertexIndex = 0;
         int visited = -1;
@@ -165,6 +246,15 @@ public class Graph {
         }
     }
 
+    /**
+     * Records the given vertexes neighbours and if they have not been visited, adds them to the stack and then
+     * visits the first neighbour popped from the stack recursively until all neighbours have been visited.
+     *
+     * @param vertex a given vertex.
+     * @param visitedNodes a list of the visited nodes.
+     * @param stack a list of all the vertexes previously visited.
+     * @return the next vertex to traverse to.
+     */
     private int traverseNodesBFS(int vertex, ArrayList<Integer> visitedNodes, ArrayList<Integer> stack) {
 
         if (this.adjacencyMatrix.length == 1) {
@@ -195,72 +285,49 @@ public class Graph {
 
     public static void main(final String[] args) {
 
-        Graph gr = new Graph(5);
-        gr.setDirection(true);
-        gr.addEdge(0, 0);
-        gr.addEdge(4, 0);
-        gr.addEdge(2, 1);
-        gr.addEdge(4, 1);
-        gr.addEdge(0, 2);
-        gr.addEdge(1, 3);
-        gr.addEdge(2, 3);
-        gr.addEdge(3, 2);
-        gr.addEdge(3, 4);
-        System.out.println(gr);
+        Graph graph1 = new Graph(5);
+        graph1.addEdge(0,1);
+        graph1.addEdge(0,3);
+        graph1.addEdge(0,4);
+        graph1.addEdge(1,0);
+        graph1.addEdge(1,2);
+        graph1.addEdge(1,4);
+        graph1.addEdge(2,1);
+        graph1.addEdge(2,3);
+        graph1.addEdge(3,0);
+        graph1.addEdge(3,2);
+        graph1.addEdge(3,4);
+        graph1.addEdge(4,0);
+        graph1.addEdge(4,1);
+        graph1.addEdge(4,3);
 
-        gr.BFS();
-        System.out.println("\n");
-
-
-        Graph G = new Graph(8);
-        G.addEdge(0, 1); //ab
-        G.addEdge(0, 4); //ae
-        G.addEdge(0, 5); //af
-        G.addEdge(1, 5); //bf
-        G.addEdge(1, 6); //bg
-        G.addEdge(2, 3); //cd
-        G.addEdge(2, 6); //cg
-        G.addEdge(3, 7); //dh
-        G.addEdge(4, 5); //ef
-        G.addEdge(6, 7); //gh
-
-        System.out.println(G);
-        G.DFS();
-        G.BFS();
+        System.out.println(graph1);
 
 
-        Graph graph = new Graph(8);
-        graph.setDirection(true);
+        Graph graph2 = new Graph(4);
+        graph2.addEdge(0,1);
+        graph2.addEdge(1,0);
+        graph2.addEdge(1,2);
+        graph2.addEdge(2,1);
+        graph2.addEdge(2,3);
+        graph2.addEdge(3,2);
 
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 2);
-        graph.addEdge(0, 4);
-        graph.addEdge(1, 0);
-        graph.addEdge(1, 3);
-        graph.addEdge(1, 5);
-        graph.addEdge(2, 0);
-        graph.addEdge(2, 3);
-        graph.addEdge(2, 6);
-        graph.addEdge(3, 1);
-        graph.addEdge(3, 2);
-        graph.addEdge(3, 7);
-        graph.addEdge(4, 0);
-        graph.addEdge(4, 5);
-        graph.addEdge(4, 6);
-        graph.addEdge(5, 1);
-        graph.addEdge(5, 4);
-        graph.addEdge(5, 7);
-        graph.addEdge(6, 2);
-        graph.addEdge(6, 4);
-        graph.addEdge(6, 7);
-        graph.addEdge(7, 3);
-        graph.addEdge(7, 5);
-        graph.addEdge(7, 6);
+        System.out.println(graph2);
 
-        System.out.println(graph);
+        Graph graph3 = new Graph(6);
+        graph3.addEdge(0,2);
+        graph3.addEdge(0,4);
+        graph3.addEdge(1,3);
+        graph3.addEdge(1,5);
+        graph3.addEdge(2,0);
+        graph3.addEdge(2,4);
+        graph3.addEdge(3,1);
+        graph3.addEdge(3,5);
+        graph3.addEdge(4,0);
+        graph3.addEdge(4,2);
+        graph3.addEdge(5,1);
+        graph3.addEdge(5,3);
 
-        graph.BFS();
-
-
+        System.out.println(graph3);
     }
 }
